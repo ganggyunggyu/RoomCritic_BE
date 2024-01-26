93fb8643a26a9e936f29b809db2ee3008752c917
강경규 <qwzx16@naver.com>
2024-01-26 23:29:15 +0900

    FEAT: BE & FE 콘텐츠 타입별 리뷰들 보여주기 기능 추가

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,5 +1,3 @@
-/** @format */
-
 import express from 'express';
 import Post from '../Models/PostModel.js';
 import User from '../Models/UserModel.js';
@@ -7,9 +5,7 @@ import User from '../Models/UserModel.js';
 const router = express.Router();
 
 router.post('/create', async (req, res) => {
-  console.log(req.body);
   try {
-    console.log(req.body);
     const reviewData = req.body.reviewData;
     const newPost = new Post({
       userId: reviewData.userId,
@@ -31,40 +27,24 @@ router.post('/create', async (req, res) => {
   }
 });
 
-router.delete('/delete', async (req, res) => {
-  console.log(req.body);
-});
+router.delete('/delete', async (req, res) => {});
 
 router.post('/review', async (req, res) => {
-  console.log(req.body);
   const posts = await Post.find({
     contentId: req.body.contentId,
     contentType: req.body.contentType,
   });
-  console.log(posts);
-
-  return res.status(200).json({ reviews: posts });
+  return res.status(200).json({
+    reviews: posts,
+  });
 });
+//홈화면에 보여줄 리뷰
 router.get('/review', async (req, res) => {
   try {
-    // 중복되지 않은 고유한 영화 이름 목록 가져오기
-    const uniqueMovieNames = await Post.aggregate([
-      {
-        $group: {
-          _id: '$contentName',
-          latestReview: { $first: '$$ROOT' },
-        },
-      },
-    ]);
-    console.log(uniqueMovieNames);
-    // 최신 10개 리뷰 가져오기
-    const latestReviews = uniqueMovieNames
-      .sort((a, b) => b.latestReview.createTime - a.latestReview.createTime)
-      .slice(0, 10)
-      .map((item) => item.latestReview);
+    const posts = await Post.find();
 
     return res.status(200).json({
-      reviews: latestReviews,
+      reviews: posts,
     });
   } catch (error) {
     return res.status(500).json({ error: 'Internal Server Error' });
@@ -87,8 +67,23 @@ router.get('/review/:userId/:reviewId', async (req, res) => {
   console.log(req.params.reviewId);
   const reviewId = req.params.reviewId;
   const post = await Post.findById(reviewId);
-  console.log(post);
-  res.status(200).json({ review: post });
+
+  return res.status(200).json({ review: post });
+});
+
+router.get('/review/tv', async (req, res) => {
+  const posts = await Post.find({ contentType: 'tv' });
+
+  console.log(posts);
+
+  return res.status(200).json({ tvContentReviews: posts });
+});
+router.get('/review/movie', async (req, res) => {
+  const posts = await Post.find({ contentType: 'movie' });
+
+  console.log(posts);
+
+  return res.status(200).json({ movieContentReviews: posts });
 });
 
 export default router;
