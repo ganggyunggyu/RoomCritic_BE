645f349275f37cfa46a83569967de524e00ccf0d
강경규 <qwzx16@naver.com>
2024-02-27 23:17:17 +0900

    FEAT: 카테고리 별 리뷰 get요청 응답 작업

---
+++ src/routes/movie.js
@@ -0,0 +1,60 @@
+import express from 'express';
+
+import Tv from '../Models/TvModel.js';
+import Review from '../Models/ReviewModel.js';
+import Movie from '../Models/MovieModel.js';
+
+const router = express.Router();
+
+router.get('/latest', async (req, res) => {
+  const content_ids = await Review.distinct('contentId');
+
+  const movies = [];
+  for (const id of content_ids) {
+    const movie = await Movie.findOne({ _id: id });
+    if (movie) {
+      movies.push(movie);
+    }
+  }
+
+  return res.status(200).json({ movies: movies });
+});
+
+router.get('/owner', async (_, res) => {
+  const movieIds = [
+    '65dbf74e13f52a0d8acd89a7',
+    '65dbf75f13f52a0d8acd949e',
+    '65dbf75313f52a0d8acd8cf4',
+    '65dbf74e13f52a0d8acd8a02',
+    '65dbf82713f52a0d8acde4bf',
+    '65dc9438c4fbf075685f539b',
+    '65dbf74d13f52a0d8acd895d',
+  ];
+  const movies = [];
+  for (const id of movieIds) {
+    const movie = await Movie.findOne({ _id: id });
+    if (movie) movies.push(movie);
+  }
+
+  return res.status(200).json({ movies: movies });
+});
+
+router.get('/top-rated-movies', async (req, res) => {
+  const topRatedMovieGate = [
+    { $sort: { vote_count: -1 } },
+    { $limit: 100 },
+    { $sort: { vote_average: -1 } },
+    { $limit: 20 },
+  ];
+  try {
+    const topRatedMovies = await Movie.aggregate(topRatedMovieGate);
+
+    console.log(topRatedMovies);
+    res.status(200).json({ movies: topRatedMovies });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+export default router;
---
+++ src/routes/tv.js
@@ -0,0 +1,41 @@
+import express from 'express';
+
+import Tv from '../Models/TvModel.js';
+import Review from '../Models/ReviewModel.js';
+const router = express.Router();
+
+router.get('/latest', async (req, res) => {
+  const content_ids = await Review.distinct('contentId');
+
+  const tvs = [];
+  for (const id of content_ids) {
+    const tv = await Tv.findOne({ _id: id });
+    if (tv) {
+      tvs.push(tv);
+    }
+  }
+  return res.status(200).json({ tvs: tvs });
+});
+
+router.get('/owner', async (_, res) => {
+  const tvIds = [
+    '65dbf5b781cd24a164a54ca9',
+    '65dbf55781cd24a164a51949',
+    '65dbf57081cd24a164a5286c',
+    '65dbf56481cd24a164a52118',
+    '65dbf54a81cd24a164a511d0',
+    '65dbf54781cd24a164a51035',
+    '65dc9ba9679160d6ac82e4ef',
+    '65dcfa7487f86005a7f80046',
+    '65dbf55981cd24a164a51a65',
+  ];
+  const tvs = [];
+  for (const id of tvIds) {
+    const tv = await Tv.findOne({ _id: id });
+    if (tv) tvs.push(tv);
+  }
+  console.log(tvs);
+  return res.status(200).json({ tvs: tvs });
+});
+
+export default router;
